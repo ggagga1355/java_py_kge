ac57c97220ea69d27281b0579c58f4375fe1c594
ggagga1355 <kkakk16@naver.com>
2025-11-25 18:14:20 +0900

    11-25

---
+++ 3.java/work/ex_1125/src/ch_13/Car.java
@@ -0,0 +1,30 @@
+package ch_13;
+
+public class Car {
+	String model;
+	String color;
+	int maxSpeed;
+	
+	
+	
+	
+	//기본 생성자
+//	public Car() {
+//		
+//	}
+	
+	
+	//값을 받아서 필드에 받는 생성자
+	public Car(String model, String color, int maxSpeed) {
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
